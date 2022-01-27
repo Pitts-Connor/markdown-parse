948783d96bda72904a963f5e7970698d062ae4e3
Pitts-Connor <connordpitts@gmail.com>
2022-01-27 13:10:22 -0700

    Test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,24 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    } 
+
+    @Test
+    public void testGetLinks() throws IOException{
+        String contents= Files.readString(Path.of("/home/connor/Documents/GitHub/markdown-parse/test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+            
     }
 }
--- test-file.md
+++ test-file.md
@@ -2,6 +2,3 @@
 
 [a link!](https://something.com)
 [another link!](some-page.html)
-[This is a link!](https://duckduckgo.com)
-![this is an image](image.jpg)
-![This is not a link, but an image!](https://wallpapercave.com/wp/VHSuCPt.jpg)
