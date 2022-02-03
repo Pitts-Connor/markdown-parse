1b9f3dee7d781f94b0d35be92b92280f2acd7d0b
Pitts-Connor <connordpitts@gmail.com>
2022-02-03 11:21:04 -0800

    Making same

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,16 +15,15 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-	    if(nextOpenBracket == -1 || nextCloseBracket ==-1 || openParen == -1 || closeParen ==-1) {
-		    break;
-	    }
-            
-	    if((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
-	       nextCloseBracket + 1 != openParen ||
-	       markdown.substring(openParen + 1, closeParen).indexOf(" ") != -1) {
-		currentIndex = closeParen + 1;
-		continue;
-	    }
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+		        break;
+	        }
+	        if((nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') ||
+	                nextCloseBracket + 1 != openParen ||
+	                markdown.substring(openParen + 1, closeParen).indexOf(" ") != -1) {
+		        currentIndex = closeParen + 1;
+		        continue;
+	        }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
